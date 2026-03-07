f073fe50cf18a6471d2418986964fc7496fc163d
nastiafill03 <149972381+nastiafill03@users.noreply.github.com>
2026-03-07 18:14:46 +0100

    Delete src directory

--- src/TodoItem.js
+++
@@ -1,9 +0,0 @@
-function TodoItem({ todo, deleteTodo }) {
-  return (
-    <li>
-      {todo.name} <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-    </li>
-  );
-}
-
-export default TodoItem;
